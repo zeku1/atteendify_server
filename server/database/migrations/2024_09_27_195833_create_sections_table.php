0896e97039aadc460888668a53b142eec8e3b512
Ezekiel Kim Lucena <zekielucena@gmail.com>
2024-10-01 14:30:50 +0800

    set migration and model

---
+++ server/database/migrations/2024_09_27_195833_create_sections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->string('section_name');
+            $table->foreignId('teacher_id')
+                ->nullable()
+                ->references('id')
+                ->on('teachers')
+                ->onDelete('set null');
+            $table->year('year');
+            $table->string('semester');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
